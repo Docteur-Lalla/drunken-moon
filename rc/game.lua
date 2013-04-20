61015e05eaf6f8930ae4b16883b6e85736446db7
Docteur Lalla <shmirnoff@gmail.com>
2013-04-20 10:58:55 +0200

    Try to represent a bullet in Lua.

--- rc/game.lua
+++ rc/game.lua
@@ -1,3 +1,20 @@
+function new_bullet(fx, fy, fr, sk, dt)
+  ball = { complex = true,
+           x = fx,
+           y = fy,
+	   r = fr,
+	   skin = sk,
+	   life = dt }
+
+  return ball
+end
+
 function game()
   hsPrint("heeey")
+
+  local fx = function(t) return 2*t + 1 end
+  local fy = function(t) return 3*t - 4 end
+  local fr = function(t) return 3 end
+
+  local bullet = new_bullet(fx, fy, fr, "bullet1", 1000)
 end
